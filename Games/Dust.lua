99bd313cd82762233e88edf6a4cbac39217a9425
ArtixSoftware <75815567+ArtixSoftware@users.noreply.github.com>
2021-03-15 17:53:40 -0500

    Update Dust.lua

--- Games/Dust.lua
+++ Games/Dust.lua
@@ -229,7 +229,7 @@ function ESPAttachToPlayer(APlayer)
                     local Pass = true
                     local APlayerVisCheck = VisibleCheck(APlayer)
                     if HUB.Visuals.Player.DistanceCheck then
-                        if (Player.Character.PrimaryPart.Position - APlayer.Character.PrimaryPart.Position).Magnitude < HUB.Visuals.Player.DistanceAllowed then else
+                        if Player.Character and Player.Character.PrimaryPart and APlayer.Character and APlayer.Character.PrimaryPart (Player.Character.PrimaryPart.Position - APlayer.Character.PrimaryPart.Position).Magnitude < HUB.Visuals.Player.DistanceAllowed then else
                             Pass = false
                         end
                     end
